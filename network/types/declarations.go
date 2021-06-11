0b451842588b4c6dc0f57e33c266c6ebf317d066
No U <just.another@email.com>
2021-06-11 11:20:15 -0600

    Changing this so string conversions are much easier

--- network/types/declarations.go
+++ network/types/declarations.go
@@ -3,7 +3,7 @@ package types
 type (
 	String string
 	VariableInteger int32 //Why is this needed? Verifying the integrity of a packet, of course.
-	Byte uint8
+	Byte byte
 	Float float32
 	Integer int32
 	ByteArray []Byte
